1d07e232498b34b6b9b285d15dfd5a9601c7063e
Bruno Santos <brunesilva@gmail.com>
2026-05-11 09:30:01 -0400

    fix: legibilidade do badge de pré-venda

--- Packages/IngressoKit/Sources/IngressoUI/Components/IngressoPreSaleBadge.swift
+++ Packages/IngressoKit/Sources/IngressoUI/Components/IngressoPreSaleBadge.swift
@@ -3,8 +3,7 @@ import SwiftUI
 struct IngressoPreSaleBadge: View {
     var body: some View {
         Text("Pré-venda".uppercased())
-            .font(.caption2.bold())
-            .foregroundStyle(IngressoColors.preSale)
+            .font(.caption2.weight(.heavy))
             .padding(.horizontal, IngressoSpacing.sm)
             .padding(.vertical, IngressoSpacing.xxs)
             .glassEffect(.regular, in: .capsule)
